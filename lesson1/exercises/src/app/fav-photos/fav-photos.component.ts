1a36d15ae2e2b3e34d5f9d7180465a8625da1b66
bhs8574 <bhs8574@gmail.com>
2020-03-07 13:54:45 -0600

    Got to class 18 checkpoint.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.shopify.com/s/files/1/0018/2052/4633/products/Blues_2019_Champs_Official_-_for_websites_800x.jpg?v=1560432246';
+  image3 = 'https://solarsystem.nasa.gov/system/downloadable_items/2842_P36254.jpg';
 
   constructor() { }
 
