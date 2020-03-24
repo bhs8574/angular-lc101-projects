12973677b22ab0b0bb9b6c53ae7971c502d39c0e
bhs8574 <bhs8574@gmail.com>
2020-03-23 19:44:07 -0500

    Finished the basic assignment, need to do bonus missions.

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -5,11 +5,76 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
 
+export class AppComponent {
+  takeOffEnabled : boolean = true;
+  title = 'Exercises: Angular Lesson 3';
+    
   color = 'green';
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  checkPosition(height, width) {
+    if (width < 0 || width > 440000 || height < 0 || height > 300000 ) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.takeOffEnabled=false;
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'shuttle in flight.';
+    }
+  }
+
+  handleLand(rocketImage) {
+    let result = window.alert('The shuttle is landing. Landing gear engaged');
+    this.takeOffEnabled=true;
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'The shuttle has landed';
+    rocketImage.style.bottom = '0px';
+  }
+
+  handleAbort(rocketImage) {
+    let result = window.confirm('Confirm that you want to abort the mission.');
+    if (result) {
+      this.takeOffEnabled=true;
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission Aborted';
+      rocketImage.style.bottom = '0px';
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) + -10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    } else if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) + -10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    }
+    this.checkPosition(this.height, this.width);
+  }
 }
+
